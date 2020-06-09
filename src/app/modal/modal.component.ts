70d5cf35ff2520656e8156bee776897ab2ce3107
Dörner Virag <mal.dorner.virag@gmail.com>
2020-06-09 10:22:05 +0200

    modal folytatása

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
-
+  form: any = {};
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  add(modalForm: NgForm, basicModal: any) {
+    //megnezni hogy letezo varosnév-e: vagy itt vagy az app-ben
+    //tovabbitani a nevet az app-nek
+    basicModal.hide();
+    modalForm.reset();
+  }
 }
